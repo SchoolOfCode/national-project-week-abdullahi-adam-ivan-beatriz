d9f63c4e5bbeca1a7717d367c42195a1cd983b67
adam960 <91432921+ah960@users.noreply.github.com>
2022-01-18 15:28:34 +0000

    - comment input and list functional

--- noob-overflow/src/App.js
+++ noob-overflow/src/App.js
@@ -1,18 +1,33 @@
 import "./App.css";
 import { testData, testComment } from "./testData";
 import QuestionExpanded from "./components/questionExpandedPost";
-
 import CommentList from "./components/questionExpandedPost/commentList";
 import { useState } from "react";
+import CommentInput from "./components/questionExpandedPost/commentInput";
 
 function App() {
   const [comments, setComments] = useState(testComment);
   const [questions, setQuestions] = useState(testData);
 
+  function addComment(comment) {
+    if (comments.includes(comment)) {
+      return;
+    }
+    setComments([...comments, comment]); // put both the author and comment as the object returned from on submit click
+  }
+
+  // function addQuestions(question) {
+  //   if (questions.includes(question)) {
+  //     return;
+  //   }
+  //   setQuestions([...questions, question]);
+  // }
+
   return (
     <div>
       <QuestionExpanded {...questions[0]} />
       <CommentList comments={comments} />
+      <CommentInput onSubmitClick={addComment} />
     </div>
   );
 }
---
+++ noob-overflow/src/components/questionExpandedPost/commentInput/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+function CommentInput({ onSubmitClick }) {
+  const [comment, setComment] = useState("");
+  const [author, setAuthor] = useState("");
+
+  function handleChangeAuthor(event) {
+    setAuthor(event.target.value);
+  }
+
+  function handleChangeComment(event) {
+    setComment(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    // Stop the page from refreshing as the form will try and submit and refresh by default
+    event.preventDefault();
+    onSubmitClick({ author: author, comment: comment });
+  }
+  return (
+    <form onSubmit={handleSubmit} className="comment-input">
+      <h3>Your Name:</h3>
+      <input onChange={handleChangeAuthor} value={author} />
+      <h3>Your Comment:</h3>
+      <input
+        onChange={(handleChangeAuthor, handleChangeComment)}
+        value={comment}
+      />
+      <button>Submit</button>
+    </form>
+  );
+}
+
+export default CommentInput;
