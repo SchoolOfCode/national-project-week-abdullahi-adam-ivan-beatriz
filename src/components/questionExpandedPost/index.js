44da99ab2e75ff87f8a8b3740edbd75c44f33fd3
Abdullahi Ali <ali259abdullahi@gmail.com>
2022-01-20 10:46:42 +0000

    fully working mvp

--- src/components/questionExpandedPost/index.js
+++ src/components/questionExpandedPost/index.js
@@ -15,7 +15,6 @@ function QuestionExpanded({
 }) {
   return (
     <div className="questionPost">
-<<<<<<< HEAD
       <section className="question-section">
         <h2>Title: {title}</h2>
         <h3>Author: {name}</h3>
@@ -33,15 +32,6 @@ function QuestionExpanded({
           onSubmitClick={onSubmitClick}
         />
       </section>
-=======
-      <h2 className="title">Title: {title}</h2>
-      <h3 className="author">Author: {name}</h3>
-      <p className="question">Question: {question}</p>
-      <h4>Do you have code?</h4>
-      <code>{code}</code>
-      <p>What have you tried already? {triedAlready}</p>
-      <p>What dont you understand?{whatDontUnderstand}</p>
->>>>>>> 1c7d048f9f8ec2e438c576965bbe8febd2c9617c
     </div>
   );
 }
